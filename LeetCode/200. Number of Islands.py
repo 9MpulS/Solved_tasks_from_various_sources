dcb5b8fc5b72f58ff299c859ebdb3d6f52fcec5a
Vlad <95881305+9MpulS@users.noreply.github.com>
2023-09-11 22:37:13 +0300

    Update 200. Number of Islands.py

--- LeetCode/200. Number of Islands.py
+++ LeetCode/200. Number of Islands.py
@@ -1,16 +1,17 @@
 import random
 
-m = int(input('Enter length of grid: '))
+m = int(input('Enter length of grid: '))    # enter length and height
 n = int(input('Enter height of grid: '))
 
 grid = [[int(random.randint(0, 1)) for j in range(n)] for i in range(m)]
-print('Your m x n 2D binary grid: ', '')
+
+print('Your m x n 2D binary grid: ', '')    # print random grid
 for j in range(n):
     for i in range(m):
         print(grid[i][j], end=' ')
     print('')
 
-def numIsland(grid, n, m):
+def numIsland(grid, n, m):  # function to count the number of islands
     count = 0
     for j in range(n):
         for i in range(m):
@@ -18,12 +19,12 @@ def numIsland(grid, n, m):
                 count += 1
                 clearRestOfLand(grid, j, i)
     return count
-def clearRestOfLand(grid, j, i):
+def clearRestOfLand(grid, j, i):    # function to depth-first search
     if j < 0 or i < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == 0:
         return
 
-    grid[i][j] = 0
-    clearRestOfLand(grid, j + 1, i)
+    grid[i][j] = 0  # mark the current land as visited
+    clearRestOfLand(grid, j + 1, i) # explore adjacent lands
     clearRestOfLand(grid, j - 1, i)
     clearRestOfLand(grid, j, i - 1)
     clearRestOfLand(grid, j, i + 1)
